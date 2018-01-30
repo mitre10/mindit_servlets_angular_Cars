07c3d9e04fdafe15aecaf36b97789d54fa2b451a
Cristian Mitre <cristian.mitre@mindit.io>
2018-01-30 22:26:45 +0200

    added delete button, functional

--- src/main/webapp/src/pages/car/list/js/carList.controller.js
+++ src/main/webapp/src/pages/car/list/js/carList.controller.js
@@ -15,6 +15,29 @@
             }, function () {
                 $scope.cars = [];
             });
+
+        $scope.delete = function(index) {
+            var id = $scope.cars[index].id;
+            CarService.delete(id)
+                .then(function(res){
+                    CarService.list()
+                        .then(function(res) {
+                            $scope.cars = res.data;
+                        }, function() {
+                            $scope.cars = [];
+                        });
+                });
+        };
+
+        $scope.car = {
+            id: '',
+            name: '',
+            type: '',
+            fuel: '',
+            price: ''
+        };
+
+        $scope.cars = [];
     }
 
 })();
